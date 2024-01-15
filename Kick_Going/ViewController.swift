d6bb564d9c6c3635c821ae37c098fb8ece00a08f
JasonYang <didwnsgus@gmail.com>
2024-01-15 19:13:35 +0900

    Initial Commit

---
+++ Kick_Going/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kick_Going
+//
+//  Created by Jason Yang on 1/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
